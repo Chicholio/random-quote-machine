b76a4ca52836d60ede43869468a319e753e509e5
Mauricio Londoño Cianci <chicholoncia@gmail.com>
2021-05-23 21:48:17 -0500

    eliminando componente MostrarFrases para dejar todo en el componente App

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,11 +1,95 @@
 import React from 'react'
+import styled from 'styled-components'
+import { FrasesDB } from '../dataBase/FrasesDB'
 
-const App = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+const DivApp = styled.div`
+  display: flex;
+  height: 100vh;
+  justify-content: center;
+  align-items: center;
+
+`
+
+const DivPrincipal = styled.div`
+  padding:20px;
+` 
+
+const DivButtons = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin: 20px 30px 10px 30px;
+`
+
+const DivSentences = styled.div`
+    font-size: 30px;
+    font-weight: bold;
+    margin-bottom: 15px;
+`
+
+const DivAuthor = styled.div`
+    text-align: right;
+    font-size: 20px;
+`
+
+const ButtonAlea = styled.div`
+  border: none;
+  padding: 10px;
+  color: white;
+  background: black;
+  border-radius: 20px;
+`
+
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      color: "",
+      sentence: "",
+      author: ""
+    }
+  }
+
+  frasesAlea = () => {
+    const cambiante = Math.round(Math.random() * 19)
+
+    this.setState({
+      color: FrasesDB[cambiante]["color"],
+      sentence: FrasesDB[cambiante]["sentence"],
+      author: FrasesDB[cambiante]["author"]
+    })
+  }
+
+  componentDidMount() {
+    this.frasesAlea()
+  }
+
+  render() {
+    return (
+      <DivApp style={{ backgroundColor: `${this.state.color}`, padding: '0px 20px' }}>
+        <DivPrincipal style={{ backgroundColor: 'white', borderRadius: '20px', width: '80%' }}>
+          <div id="quote-box" style={{ color: `${this.state.color}` }}>
+            <DivSentences id="text" style={{ fontSize: 'Arial', color: `${this.state.color}` }}>
+              {this.state.sentence}
+            </DivSentences>
+            <DivAuthor id="quote-autor" style={{ fontSize: 'Arial', color: `${this.state.color}` }}>
+              {this.state.author}
+            </DivAuthor>
+            <DivButtons>
+              <div>
+                <button style={{ background: 'transparent', border: 'none' }}><a href={`https://twitter.com/intent/tweet/?text="${this.state.sentence}${this.state.author}"`} id="tweet-quote" target="_blanck"><img src="https://i.ibb.co/7RgdVpC/twitter.png" alt="twitter" style={{ width: '40px' }} /></a></button>
+              </div>
+              <div>
+                <ButtonAlea id="new-quote" onClick={this.frasesAlea}>Aleatoria</ButtonAlea>
+              </div>
+            </DivButtons>
+          </div>
+        </DivPrincipal>
+      </DivApp>
+    )
+  }
 }
 
 export default App
---
+++ src/dataBase/FrasesDB.js
@@ -0,0 +1,122 @@
+export const FrasesDB = [
+    {
+        "id": 0,
+        "sentence": "«La libertad, Sancho, es uno de los más preciosos dones que a los hombres dieron los cielos; con ella no pueden igualarse los tesoros que encierra la tierra ni el mar encubre.»",
+        "author": "- Miguel de Cervantes Saavedra",
+        "color":"#D4E157"
+    },
+    {
+        "id": 1,
+        "sentence": "«Creer que un cielo en un infierno cabe, dar la vida y el alma a un desengaño; esto es amor, quien lo probó lo sabe.»",
+        "author": "- Lope de Vega",
+        "color":"#9CCC65"
+    },
+    {
+        "id": 2,
+        "sentence": "«El secreto de la felicidad, o por lo menos de la tranquilidad, es saber separar el sexo del amor.»",
+        "author": "- Pedro Calderón de la Barca",
+        "color":"#66BB6A"
+    },
+    {
+        "id": 3,
+        "sentence": "«Libertad es el derecho que todo hombre tiene a ser honrado y a pensar y a hablar sin hipocresía.»",
+        "author": "- José Martí",
+        "color":"#26A69A"
+    },
+    {
+        "id": 4,
+        "sentence": "«Si no te conozco, no he vivido; si muero sin conocerte, no muero, porque no he vivido.»",
+        "author": "- Luis Cernuda",
+        "color":"#26C6DA"
+    },
+    {
+        "id": 5,
+        "sentence": "«Ahora yo no sé si vas a poder leer esta carta, pero igual siento como una necesidad de decirte que yo contigo he sido más feliz de lo que los libros dicen que se puede.»",
+        "author": "- Eduardo Galeano",
+        "color":"#29B6F6"
+    },
+    {
+        "id": 6,
+        "sentence": "«Siempre es levemente siniestro volver a los lugares que han sido testigos de un instante de perfección.»",
+        "author": "- Ernesto Sabato",
+        "color":"#42A5F5"
+    },
+    {
+        "id": 7,
+        "sentence": "«Y aunque no siempre he entendido mis culpas y mis fracasos, en cambio sé que, en tus brazos, el mundo tiene sentido.»",
+        "author": "- Mario Benedetti",
+        "color":"#5C6BC0"
+    },
+    {
+        "id": 8,
+        "sentence": "«Como si se pudiera elegir el amor, como si no fuera un rayo que te parte los huesos y te deja estaqueado en mitad del patio.»",
+        "author": "- Julio Cortázar",
+        "color":"#7E57C2"
+    },
+    {
+        "id": 9,
+        "sentence": "«Vivos sin vivir en mí, que tan alta vida espero que muero porque no muero.»",
+        "author": "- Santa Teresa de Jesús",
+        "color":"#AB47BC"
+    },
+    {
+        "id": 10,
+        "sentence": "«Mujer el mundo está amueblado por tus ojos. Se hace más alto el cielo en tu presencia. La tierra se prolonga de rosa en rosa. Y el aire se prolonga de paloma en paloma.»",
+        "author": "- Vicente Huidobro",
+        "color":"#7fecbe"
+    },
+    {
+        "id": 11,
+        "sentence": "«¿Hay algo, pregunto yo, más noble que una botella de vino bien conservado entre dos almas gemelas?»",
+        "author": "- Nicanor Parra",
+        "color":"#EC407A"
+    },
+    {
+        "id": 12,
+        "sentence": "«Hacer el amor en una estrella y sentir que me sobra vida y me faltan brazos.»",
+        "author": "- Dulce Chacón",
+        "color":"#EDBB99 "
+    },
+    {
+        "id": 13,
+        "sentence": "«Mi reino por un 'te amo', sangrándome en la boca, mi eternidad por sólo dos palabras.»",
+        "author": "- José Hierro",
+        "color":"#F5CBA7"
+    },
+    {
+        "id": 14,
+        "sentence": "«¿Cuándo crees que muere una persona? ¿Cuando su corazón es atravesado por una bala? No. ¿Cuando sucumbe a una enfermedad incurable? No. ¿Cuando bebe una sopa con setas venenosas? ¡NO! Una persona muere cuando es olvidada...»",
+        "author": "- One Piece, Hiriluk",
+        "color":"#FAD7A0"
+    },
+    {
+        "id": 15,
+        "sentence": "«Si se preocuparan por algo tan trivial como perder el respeto, entonces es mejor no tenerlo en primer lugar.»",
+        "author": "- One Piece, Fujitora",
+        "color":"#F9E79F"
+    },
+    {
+        "id": 16,
+        "sentence": "«Debemos estar orgullosos de lo que hicimos, demostramos que con esfuerzo puedes conseguirlo todo.»",
+        "author": "- Black Clover, Yuno",
+        "color":"#ABEBC6"
+    },
+    {
+        "id": 17,
+        "sentence": "«No vivas con falsedades ni miedos, porque terminarás odiándote a ti mismo.»",
+        "author": "- Uzumaki Naruto",
+        "color":"#A9DFBF"
+    },
+    {
+        "id": 18,
+        "sentence": "«No importa cuán capacitado que estés, tú solo no puedes cambiar el mundo. Pero eso es lo maravilloso del mundo.»",
+        "author": "- Death Note, L",
+        "color":"#A2D9CE"
+    },
+    {
+        "id": 19,
+        "sentence": "«Si no vives para servir no sirves para vivir.»",
+        "author": "- Robert Baden-Powell",
+        "color":"#A3E4D7"
+    }
+]
